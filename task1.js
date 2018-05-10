6de64a6ee8406b0594091b37ab4fb434b0026603
Alenapadsekina <33716680+Alenapadsekina@users.noreply.github.com>
2018-05-10 13:47:42 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,7 +1,11 @@
-var salaries = {Jonh: 600, Peter: 400, Sam: 500};
+var salaries = {
+    Jonh: 600,
+    Peter: 400,
+    Sam: 500
+};
 var sum = 0;
 for (var key in salaries) {
-	if (salaries[key] > 0) {
-		sum = sum + salaries[key];
-		}
+    if (salaries[key] > 0) {
+        sum = sum + salaries[key];
+    }
 }
