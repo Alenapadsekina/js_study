aba434f2fa0bb8ff5d327a496e2e7b4de9870cfc
Alenapadsekina <33716680+Alenapadsekina@users.noreply.github.com>
2018-05-10 14:41:01 +0300

    Update Task3.js
    1) Added ; after break
    2) Changed tabulation
    3)Changed the check's position

--- Task3.js
+++ Task3.js
@@ -1,25 +1,25 @@
 var employees = [{
-	name: "John",
-	salary: 600,
-	age: 50
+    name: "John",
+    salary: 600,
+    age: 50
 }, {
-	name: "Peter",
-	salary: 400,
-	age: 30
+    name: "Peter",
+    salary: 400,
+    age: 30
 }, {
-	name: "Sam",
-	salary: 500,
-	age: 40
+    name: "Sam",
+    salary: 500,
+    age: 40
 }];
 
 for (var i = 0; i < employees.length; i++) {
-var count = 0;
-	if (employees[i].name === "John") {
-	count++;
-	console.log("Yes");
-	break
-	} 
+    var count = 0;
+    if (employees[i].name === "J") {
+        count++;
+        console.log("Yes");
+        break;
+    }
+}
 if (count === 0) {
-	console.log("No")
-	}
+    console.log("No")
 }
