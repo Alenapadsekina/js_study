7c47a445a039735a7091b851d780a8aaa2dc91f8
Alenapadsekina <alena.padsekina@itechart-group.com>
2018-06-12 17:04:30 +0300

    HTML and JS docs updates
    HTML doc:
    1) apply onclick to select (not every option)
    JS doc:
    1) place cleanDropdown and cleanOldResult inside other functions
    2) replace '==' with '==='
    3) rename function, that displays errors

--- Website/employeesAvailability.js
+++ Website/employeesAvailability.js
@@ -30,23 +30,8 @@ var employees = [
 ];
 
 
-function cleanDropdown() { // clean previously filled 'Members' dropdown
-    var oldGroup = document.getElementById("teamMembers");
-    while (oldGroup.firstChild) {
-        oldGroup.removeChild(oldGroup.firstChild);
-    }
-}
 
-function cleanOldResult() { // erase the previously created table or error message
-    var oldTable = document.getElementById("reportTable");
-    while (oldTable.firstChild) {
-        oldTable.removeChild(oldTable.firstChild);
-    }
-    var oldError = document.getElementById("errorMessage");
-    while (oldError.firstChild) {
-        oldError.removeChild(oldError.firstChild);
-    }
-}
+
 
 
 function defineGroup() { // create an array containing team members
@@ -56,7 +41,7 @@ function defineGroup() { // create an array containing team members
     function defineTeamMembers(team) { // populate 'group' array with corresponding team members
 
         for (var i = 0; i < employees.length; i++) {
-            if (employees[i].team == team) {
+            if (employees[i].team === team) {
                 group.push(employees[i]);
             }
         }
@@ -64,15 +49,21 @@ function defineGroup() { // create an array containing team members
         console.log(group);
 
         function populateDropdown() {
-
+            function cleanDropdown() { // clean previously filled 'Members' dropdown
+                var oldGroup = document.getElementById("teamMembers");
+                while (oldGroup.firstChild) {
+                    oldGroup.removeChild(oldGroup.firstChild);
+                }
+            }
             cleanDropdown();
 
-            function populateTeamMembers() { // create lines in 'Members' dropdown and popoulate them with group members
+            function populateTeamMembers() { // create lines in 'Members' dropdown and populate them with group members
+                var select = document.getElementById("teamMembers");
                 for (var i = 0; i < group.length; i++) {
                     var option = document.createElement("option");
                     option.text = group[i].name;
                     option.value = group[i].name;
-                    var select = document.getElementById("teamMembers");
+                    
                     select.appendChild(option);
                 }
             }
@@ -92,9 +83,8 @@ function defineGroup() { // create an array containing team members
 function createTable() { // create a table corresponding to the selected team
     console.log("inside create table");
     var group = defineGroup();
-
+    var table = document.getElementById("reportTable");
     for (var i = 0; i < group.length; i++) {
-        var table = document.getElementById("reportTable");
         var row = table.insertRow(i);
         var nameCell = row.insertCell(0);
         var statusCell = row.insertCell(1);
@@ -108,7 +98,6 @@ function createTable() { // create a table corresponding to the selected team
             statusCell.classList.add("notAvailable");
         }
     }
-    var table = document.getElementById("reportTable");
     var headerRow = table.insertRow(0);
     headerRow.innerHTML = "Employees availability";
     headerRow.classList.add("headerRow");
@@ -119,7 +108,7 @@ function createTable() { // create a table corresponding to the selected team
 
 var errorMessage;
 
-function validation(errorMessage) { // create error message
+function showError(errorMessage) { // create error message
     var error = document.createTextNode(errorMessage);
     var div = document.getElementById("errorMessage");
     div.appendChild(error);
@@ -137,6 +126,16 @@ function selectTeam() { // populate 'Members' dropdown
 
 function generateReport() { // show report: a table or error message
 
+    function cleanOldResult() { // erase the previously created table or error message
+        var oldTable = document.getElementById("reportTable");
+        while (oldTable.firstChild) {
+            oldTable.removeChild(oldTable.firstChild);
+        }
+        var oldError = document.getElementById("errorMessage");
+        while (oldError.firstChild) {
+            oldError.removeChild(oldError.firstChild);
+        }
+    }
     cleanOldResult();
     console.log("clean old result OK");
     var team = document.getElementById("teams").value;
@@ -144,7 +143,7 @@ function generateReport() { // show report: a table or error message
     switch (team) {
         case "":
             errorMessage = "Please define a team";
-            validation(errorMessage);
+            showError(errorMessage);
             break;
         case "HTML5/IFE":
         case "Main releases":
@@ -154,7 +153,7 @@ function generateReport() { // show report: a table or error message
             break;
         default:
             errorMessage = "No data found";
-            validation(errorMessage);
+            showError(errorMessage);
             break;
     }
 }
